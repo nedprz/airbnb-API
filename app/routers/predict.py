7dc57ed9ebd3204a6b1d7f720e3a9b4b2680051b
nedprz <16900857+nedprz@users.noreply.github.com>
2020-06-25 08:54:14 -0700

    new fields

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -12,18 +12,19 @@ router = APIRouter()
 class Item(BaseModel):
     """Use this data model to parse the request body JSON."""
 
-    SquareFootage: float = Field(..., example=3000)
+    
     ZipCode: int = Field(..., example=11215)
-    String: str = Field(..., example='yes')
+    RoomType: str = Field(..., example='Entire home/apt')
+    Accomodates: int = Field(..., example=3)
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
-    @validator('SquareFootage')
-    def SquareFootage_must_be_positive(cls, value):
-        """Validate that SquareFootage is a positive number."""
-        assert value > 0, f'SquareFootage == {value}, must be > 0'
+    @validator('ZipCode')
+    def ZipCode_must_be_positive(cls, value):
+        """Validate that ZipCode is a positive number."""
+        assert value > 0, f'ZipCode == {value}, must be > 0'
         return value
 
 
