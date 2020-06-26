ab85e0ab1ffae7e04ce118541798146e968f0e82
nedprz <16900857+nedprz@users.noreply.github.com>
2020-06-25 17:09:30 -0700

    b

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -13,24 +13,24 @@ from flask import Flask, jsonify, request
 
 log = logging.getLogger(__name__)
 router = APIRouter()
-# model = load('model.pkl')
+model = load('model.pkl')
 
 class Item(BaseModel):
     """Use this data model to parse the request body JSON."""
 
-    
-    ZipCode: int = Field(..., example=11215)
+    Neighborhood: str = Field(..., example='Park Slope')
     RoomType: str = Field(..., example='Entire home/apt')
+    MinimumNights: int = Field(..., example = 5)
     Accomodates: int = Field(..., example=3)
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
-    @validator('ZipCode')
-    def ZipCode_must_be_positive(cls, value):
-        """Validate that ZipCode is a positive number."""
-        assert value > 0, f'ZipCode == {value}, must be > 0'
+    @validator('MinimumNights')
+    def MinimumNights_must_be_positive(cls, value):
+        """Validate that MinimumNights is a positive number."""
+        assert value > 0, f'MinimumNights == {value}, must be > 0'
         return value
 
 
@@ -47,26 +47,25 @@ async def predict(item: Item):
 
 
 
-#@router.get('/model')
-#async def model1():
- #   # get data
-  #  data = request.get_json(force=True)
-   # print(data)
-#
-    # convert data into dataframe
- #   data.update((x, [y]) for x, y in data.items())
-  #  data_df = pd.DataFrame.from_dict(data)
+@router.post('/predict2')
+def predict2():
+    # get data
+    data = request.get_json(force=True)
+    print(data)
 
-   # print(data_df.shape)
+    # convert data into dataframe
+    data.update((x, [y]) for x, y in data.items())
+    data_df = pd.DataFrame.from_dict(data)
+
+    print(data_df.shape)
     # predictions
-   # result = model.predict(data_df)
+    result = model.predict(data_df)
 
     # send back to browser
-   # output = {'results': int(result[0])}
+    output = {'results': int(result[0])}
 
     # return data
-   # return jsonify(results=output)
-
+    return jsonify(results=output)
 
 
 
